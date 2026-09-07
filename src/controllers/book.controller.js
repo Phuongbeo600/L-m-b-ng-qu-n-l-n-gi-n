2494bb7f3c5ea55ce40432cbac9a39f02e08f6b8
Bùi Nguyễn Phương <25023518@vnu.edu.vn>
2026-09-07 16:20:26 +0700

    SỬa con troller theo bất đồng bộ database

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,23 +1,43 @@
 
 const bookModel = require('../models/book.model');
 
-exports.getAllBooks = (req, res) => {
-    const books = bookModel.getAll();
-    res.json(books);
+exports.getAllBooks = async (req, res) => {
+    try {
+        const books = await bookModel.getAll();
+        res.json(books);
+    } catch (error) {
+        console.error("Lỗi:", error);
+        res.status(500).json({ message: "Lỗi lấy danh sách " });
+    }
 };
 
-exports.create = (req, res) => {
-    const newBook = bookModel.create(req.body);
-    res.json({ message: " Thành công ", data: newBook });
+exports.create = async (req, res) => {
+    try {
+        const { author, title } = req.body;
+
+        if (!author || !title) {
+            return res.status(400).json({ message: "Vui lòng nhập đầy đủ thông tin" });
+        }
+
+        const newBook = await bookModel.create({ author, title });
+    } catch (error) {
+        console.error("Lỗi:", error);
+        res.status(500).json({ message: " Lỗi thêm sách" });
+    }
 };
 
-exports.deleteBook = (req, res) => {
-    const id = Number(req.params.id);
-    const success = bookModel.remove(id);
+exports.deleteBook = async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const success = await bookModel.remove(id);
 
-    if (success) {
-        res.json({ message: "xoá thành công !" });
-    } else {
-        res.status(404).json({ message: "Không tìm thấy sách để xóa!" });
+        if (!success) {
+            return res.status(404).json({ message: "Không tìm thấy sách để xóa" });
+        }
+
+        res.json({ message: "Xóa thành công" });
+    } catch (error) {
+        console.error("Lỗi:", error);
+        res.status(500).json({ message: "Lỗi xóa sách" });
     }
 };
